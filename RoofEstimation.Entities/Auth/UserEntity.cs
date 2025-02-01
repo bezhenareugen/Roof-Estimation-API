79ec8cf42ea0a21cdff0ef28f619b00a2a998e78
Eugen Bezhenar <eugen.bezhenar@raxwel.de>
2025-02-01 21:39:36 +0200

    Fixed quering issues

--- RoofEstimation.Entities/Auth/UserEntity.cs
+++ RoofEstimation.Entities/Auth/UserEntity.cs
@@ -6,8 +6,8 @@ namespace RoofEstimation.Entities.Auth;
 
 public class UserEntity : IdentityUser
 {
-    public RoofInfoEntity RoofInfo { get; set; }
-    public PipeInfoEntity PipeInfo { get; set; }
+    public RoofInfoEntity? RoofInfo { get; set; }
+    public PipeInfoEntity? PipeInfo { get; set; }
     public bool Gutters { get; set; } = true;
     public bool PermitFees { get; set; } = true;
 }

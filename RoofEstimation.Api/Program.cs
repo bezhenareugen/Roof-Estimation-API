2e889084553b80355e815825bfc859d5a5206ea5
Eugen Bezhenar <eugen.bezhenar@raxwel.de>
2025-02-20 21:27:00 +0200

    Added CORS

--- RoofEstimation.Api/Program.cs
+++ RoofEstimation.Api/Program.cs
@@ -60,6 +60,20 @@ builder.Services.AddScoped<ITearOffService, TearOffService>();
 builder.Services.AddScoped<IMaterialService, MaterialService>();
 builder.Services.AddScoped<IEstimationService, EstimationService>();
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("http://localhost:3000",
+                    "http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -76,7 +90,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 
-//app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseRouting();
 
